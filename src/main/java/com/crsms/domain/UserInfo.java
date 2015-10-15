f6331d33c9af1c619c12cae41f9be30bd1cf1bc7
Valerii Motresku <valeriimotresku@gmail.com>
2015-10-15 14:55:12 +0300

    Update UserInfo.java

--- src/main/java/com/crsms/domain/UserInfo.java
+++ src/main/java/com/crsms/domain/UserInfo.java
@@ -3,14 +3,46 @@ package com.crsms.domain;
 import java.util.Set;
 
 public class UserInfo {
-	Long id;
-	String firstName;
-	String secondName;
-	String email;
-	Set<Group> groups;
+	private Long id;
+	private String firstName;
+	private String secondName;
+	private Set<Group> groups;
 	
 	public UserInfo() {
 		super();
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public Set<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Set<Group> groups) {
+		this.groups = groups;
+	}
+
+	
 }
