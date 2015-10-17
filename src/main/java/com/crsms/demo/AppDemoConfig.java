a9ca669f32c3804b897be61a611078f64d589d72
Valerii Motresku <valeriimotresku@gmail.com>
2015-10-18 01:20:39 +0300

    demo with config for debugging etc.

---
+++ src/main/java/com/crsms/demo/AppDemoConfig.java
@@ -0,0 +1,17 @@
+package com.crsms.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Valerii Motresku
+ *
+ */
+
+@Configuration
+@ComponentScan(basePackages = "com.crsms")
+public class AppDemoConfig {
+
+}
+
