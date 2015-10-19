79172164c8aead77b64e10f0cb2a4eb4645a75d5
Valerii Motresku <valeriimotresku@gmail.com>
2015-10-19 21:13:47 +0300

    many to one direction

--- src/main/java/com/crsms/domain/Course.java
+++ src/main/java/com/crsms/domain/Course.java
@@ -9,8 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -51,7 +51,7 @@ public class Course {
 	@Column(nullable = false)
 	private Boolean open = false;
 	
-	@OneToOne
+	@ManyToOne
 	@Cascade({CascadeType.ALL})
     @JoinColumn(name="direction_id")
 	private Direction direction;
