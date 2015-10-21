d18d907e4f93c153addb8636b108fa09117ab45e
stefankiv <stefankiv@inbox.ru>
2015-10-21 12:09:01 +0300

    added simple controller for module's pages

---
+++ src/main/java/com/crsms/controller/ModuleController.java
@@ -0,0 +1,31 @@
+package com.crsms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.crsms.domain.Module;
+import com.crsms.service.ModuleService;
+
+@Controller
+public class ModuleController {
+	
+	@Autowired
+	ModuleService service;
+	
+	@RequestMapping(value = {"/createmodule"}, method = RequestMethod.GET)
+	public String newModule(ModelMap model) {
+		Module module = new Module();
+		model.addAttribute("module", module);
+		return "createmodule";
+	}
+	
+	@RequestMapping(value = {"/createmodule"}, method = RequestMethod.POST)
+	public String saveModule(Module module, ModelMap model) {
+		service.save(module);
+		model.addAttribute("message", "Module " + module.getName() + " created successfully");
+		return "success";
+	}
+}
