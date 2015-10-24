6708b0327acf1f8845c8f375ac2d66f504af7128
stefankiv <stefankiv@inbox.ru>
2015-10-24 22:52:00 +0300

    deleted unused imports

--- src/main/java/com/crsms/dao/ModuleDaoImpl.java
+++ src/main/java/com/crsms/dao/ModuleDaoImpl.java
@@ -5,11 +5,8 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.Criteria;
-import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
