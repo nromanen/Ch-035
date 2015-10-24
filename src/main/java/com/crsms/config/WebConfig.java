356073f5f2eb93c2089066ebe40c18c07631005f
stefankiv <stefankiv@inbox.ru>
2015-10-24 16:23:18 +0300

    added locaization configuration

--- src/main/java/com/crsms/config/WebConfig.java
+++ src/main/java/com/crsms/config/WebConfig.java
@@ -1,14 +1,19 @@
 package com.crsms.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
@@ -36,4 +41,29 @@ class WebConfig extends WebMvcConfigurationSupport {
         tilesConfigurer.setPreparerFactoryClass(org.springframework.web.servlet.view.tiles3.SpringBeanPreparerFactory.class);
         return tilesConfigurer;    
     }
+    
+    @Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = 
+							new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/resources/localization/crsms");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+    
+    @Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setDefaultLocale(new Locale("ua"));
+		resolver.setCookieName("localeCookie");
+		resolver.setCookieMaxAge(4800);
+		return resolver;
+	}
+    
+    @Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		registry.addInterceptor(interceptor);
+	}
 }
