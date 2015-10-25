1396f654f39f9e495015374e37bcfd1479164436
rromaniuk <rroman@online.ua>
2015-10-25 03:28:55 +0300

    removed preAuthority

--- src/main/java/com/crsms/dao/UserDao.java
+++ src/main/java/com/crsms/dao/UserDao.java
@@ -9,18 +9,18 @@ import com.crsms.domain.User;
 
 public interface UserDao {
 	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_TEACHER','ROLE_STUDENT')")
+
 	User saveUser(User user);
 	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_TEACHER','ROLE_STUDENT')")
+
 	User getUserById(Long id);
 	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_TEACHER','ROLE_STUDENT')")
+	
 	User getUserByEmail(String email);
 	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_TEACHER','ROLE_STUDENT')")
+
 	void delete(Long id);
 	
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_TEACHER','ROLE_STUDENT')")
+	
 	List<User> getAllUsers();
 }
