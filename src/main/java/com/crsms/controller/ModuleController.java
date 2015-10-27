e3fc2a7dc580e1c6c5caa897c00276b8d1e3e36b
stefankiv <stefankiv@inbox.ru>
2015-10-27 14:10:04 +0200

    added 'get all modules by course id' method

--- src/main/java/com/crsms/controller/ModuleController.java
+++ src/main/java/com/crsms/controller/ModuleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.crsms.domain.Module;
+import com.crsms.service.CourseService;
 import com.crsms.service.ModuleService;
 
 /**
@@ -24,16 +25,27 @@ public class ModuleController {
 	
 	private final String MODULES_PAGE = "modules";
 	private final String CREATE_MODULE_PAGE = "createmodule";
+	private final String ERROR_PAGE = "errorpage";
+	
+	private final String COURSE_DOESNT_EXISTS_MESSAGE = "Course with that ID doesn't exist: ";
 	
 	@Autowired
 	private ModuleService moduleService;
 	
+	@Autowired
+	private CourseService courseService;
+	
 	//TODO РОЗІБРАТИСЬ ЧОМУ У ВАЛЄРИ АВТОМАТИЧНО ДОДАЄ СЛЕШ '/' В КІНЦІ УРЛА 'crsms/courses/1/modules' А В МЕНЕ НІ
 	// crsms/courses/1/modules - видасть 404
 	// crsms/courses/1/modules/ - працюватиме	
-	@RequestMapping(value = {"/", "/all"}, method = RequestMethod.GET)
-	public String showModules(Model model) {
-		List<Module> modules = moduleService.getAll();
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
+	public String showModules(@PathVariable Long courseId, Model model) {
+		if (!validateCourseId(courseId)) {
+			model.addAttribute("message", COURSE_DOESNT_EXISTS_MESSAGE + courseId);
+			return ERROR_PAGE;
+		}
+		
+		List<Module> modules = moduleService.getAllByCourseId(courseId);
 		model.addAttribute("modules", modules);
 		return MODULES_PAGE;
 	}
@@ -81,4 +93,17 @@ public class ModuleController {
 	private String redirect(Long courseId) {
 		return "redirect:/courses/" + courseId + "/modules/";
 	}
+	
+	/**
+	 * Returns true if course with id 'courseId' exists in database
+	 * @param courseId course id to check
+	 * @return true if course with id 'courseId' exists in database, false if not 
+	 */
+	private boolean validateCourseId(Long courseId) {
+		if (courseService.getCourseById(courseId) == null) {
+			return false;
+		}
+		return true;
+	}
+
 }
