fdc94e039c24a085d83432b32ab15fab67bc9a87
Petro Andriets <web.it.www@gmail.com>
2015-10-28 03:51:07 +0200

    Small refactoring in Config layer.

--- src/main/java/com/crsms/config/RootConfig.java
+++ src/main/java/com/crsms/config/RootConfig.java
@@ -11,17 +11,14 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 
 @Configuration
-// @EnableWebMvc is turned off for console debug only!!!
 @EnableWebMvc
 @ComponentScan(basePackages = "com.crsms")
 @Import({ SecurityConfig.class, HibernateConfig.class })
 public class RootConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations(
-				"/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations(
-				"/webjars/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
 	}
 
 	@Override
