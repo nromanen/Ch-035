6c77127754df8b4a93110cd35138da73deaddb67
maftey <maftey@gmail.com>
2015-10-29 04:12:46 +0200

    chage lib for date ui and add localisation

--- src/main/webapp/resources/js/course/courseForm.js
+++ src/main/webapp/resources/js/course/courseForm.js
@@ -1,7 +1,11 @@
+var langeges = {'ua':'uk'};
 $(document).ready(function(e) {
 	$(function () {
-        $('#datetimepicker1').datetimepicker({
-        	format: 'DD/MM/YYYY'
+        $('#datetimepicker1').datepicker({
+            format: "dd/mm/yyyy",
+            language: langeges[$.cookie('localeCookie')],
+            autoclose: true,
+            todayHighlight: true
         });
     });
 });
