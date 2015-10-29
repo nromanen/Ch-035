a35e05571b2a4590373a1b01bb8a3e96e883b2c4
stefankiv <stefankiv@inbox.ru>
2015-10-29 11:21:38 +0200

    handling

--- src/main/java/com/crsms/controller/ExceptionController.java
+++ src/main/java/com/crsms/controller/ExceptionController.java
@@ -3,6 +3,7 @@ package com.crsms.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,7 +14,13 @@ public class ExceptionController {
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public String requestHandlingNoHandlerFound(HttpServletRequest req, NoHandlerFoundException ex) {
+    public String handlingNoHandlerFound(HttpServletRequest request, NoHandlerFoundException ex) {
         return "404";
     }
+    /*
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handlingMissingServletRequestParameter(HttpServletRequest request, MissingServletRequestParameterException ex) {
+    	return "404";
+    }*/
 }
