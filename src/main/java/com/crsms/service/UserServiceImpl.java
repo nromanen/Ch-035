b6f34beb78846831a187d4054b49c55bc125e7ad
georgekucher <gonzo-2005@yandex.ru>
2015-10-29 19:50:57 +0200

    clean up

--- src/main/java/com/crsms/service/UserServiceImpl.java
+++ src/main/java/com/crsms/service/UserServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.crsms.dao.RoleDao;
 import com.crsms.dao.UserDao;
-import com.crsms.domain.Role;
 import com.crsms.domain.User;
 
 /**
