311aa5c7cbe1d2966948725a6a90161ab6468759
rroman <rroman@online.ua>
2015-10-29 20:38:40 +0200

    added preatorize

--- src/main/java/com/crsms/config/SecurityConfig.java
+++ src/main/java/com/crsms/config/SecurityConfig.java
@@ -37,11 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	  http
 		  .authorizeRequests()
-						  	.antMatchers("/", "/hello","/login").permitAll()
+						  	.antMatchers("/login").permitAll()
 						  	.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 						  	.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-						  	.antMatchers("/teacher/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER') or hasRole('ROLE_TEACHER')")
-						  	.antMatchers("/student/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER') or hasRole('ROLE_TEACHER') or hasRole('ROLE_STUDENT')")
+						  	.antMatchers("/teacher/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_TEACHER')")
+						  	.antMatchers("/student/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_STUDENT')")
 						  	.and();
 	  http
 	  		.formLogin().loginPage("/login")
