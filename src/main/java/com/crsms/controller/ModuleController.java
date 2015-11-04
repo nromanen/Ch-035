4400854f2c132e099bb43816a3733e7331b56f42
stefankiv <stefankiv@inbox.ru>
2015-11-04 23:48:37 +0200

    added unique module name validation

--- src/main/java/com/crsms/controller/ModuleController.java
+++ src/main/java/com/crsms/controller/ModuleController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.crsms.domain.Module;
+import com.crsms.dto.ModuleFormDto;
 import com.crsms.service.CourseService;
+import com.crsms.service.DtoService;
 import com.crsms.service.ModuleService;
 import com.crsms.validator.ModuleValidator;
 
@@ -39,6 +41,9 @@ public class ModuleController {
 	private CourseService courseService;
 	
 	@Autowired
+	private DtoService dtoService;;
+	
+	@Autowired
 	private ModuleValidator validator;
 	
 	@InitBinder
@@ -53,22 +58,48 @@ public class ModuleController {
 		return MODULES_VIEW;
 	}
 	
+	@RequestMapping(value = {"/add"}, method = RequestMethod.GET)
+	public String newModule(@PathVariable Long courseId, Model model) {
+		Module module = new Module();
+		
+		ModuleFormDto moduleFormDto = dtoService.convert(module, ModuleFormDto.class, Module.class);
+		moduleFormDto.setCourseId(courseId);
+		
+		model.addAttribute("moduleFormDto", moduleFormDto);
+		return ADD_MODULE_VIEW;
+	}
+	
+	@RequestMapping(value = {"/add"}, method = RequestMethod.POST)
+	public String saveModule(@PathVariable Long courseId, @Validated ModuleFormDto moduleFormDto, BindingResult result) {
+		if (result.hasErrors()) {
+			return ADD_MODULE_VIEW;
+		}
+		Module module = dtoService.convert(moduleFormDto, Module.class, ModuleFormDto.class);
+		moduleService.add(courseId, module);
+		return redirect(courseId);
+	}	
+	
 	@RequestMapping(value = {"/{moduleId}/edit"}, method = RequestMethod.GET)
 	public String editModule(@PathVariable Long courseId, @PathVariable Long moduleId, Model model) {
 		Module module = moduleService.getById(moduleId);
-		model.addAttribute("module", module);
+		
+		ModuleFormDto moduleFormDto = dtoService.convert(module, ModuleFormDto.class, Module.class);
+		moduleFormDto.setCourseId(courseId);
+		
+		model.addAttribute("moduleFormDto", moduleFormDto);
 		return EDIT_MODULE_VIEW;
 	}
 	
 	@RequestMapping(value = {"/{moduleId}/edit"}, method = RequestMethod.POST)
 	public String updateModule(@PathVariable Long courseId, @PathVariable Long moduleId, 
-								@Validated Module module, BindingResult result) {
+								@Validated ModuleFormDto moduleFormDto, BindingResult result) {
 		if (result.hasErrors()) {
 			return EDIT_MODULE_VIEW;
 		}
-		if (moduleService.getById(moduleId) != null) {
-			moduleService.update(module);
-		}
+
+		Module module = dtoService.convert(moduleFormDto, Module.class, ModuleFormDto.class);
+		moduleService.update(module);
+
 		return redirect(courseId);
 	}
 	
@@ -77,22 +108,6 @@ public class ModuleController {
 		moduleService.deleteById(moduleId);
 		return redirect(courseId);
 	}
-	
-	@RequestMapping(value = {"/add"}, method = RequestMethod.GET)
-	public String newModule(@PathVariable Long courseId, Model model) {
-		Module module = new Module();
-		model.addAttribute("module", module);
-		return ADD_MODULE_VIEW;
-	}
-	
-	@RequestMapping(value = {"/add"}, method = RequestMethod.POST)
-	public String saveModule(@PathVariable Long courseId, @Validated Module module, BindingResult result) {
-		if (result.hasErrors()) {
-			return ADD_MODULE_VIEW;
-		}
-		moduleService.add(courseId, module);
-		return redirect(courseId);
-	}	
 	
 	/**
 	 * Returns redirection path
--- src/main/java/com/crsms/validator/ModuleValidator.java
+++ src/main/java/com/crsms/validator/ModuleValidator.java
@@ -1,18 +1,24 @@
 package com.crsms.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.crsms.domain.Module;
+import com.crsms.dto.ModuleFormDto;
+import com.crsms.service.CourseService;
 
 @Component
 public class ModuleValidator implements Validator {
 	
+	@Autowired
+	private CourseService courseService;
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
-		return Module.class.equals(clazz);
+		return ModuleFormDto.class.equals(clazz);
 	}
 
 	@Override
@@ -20,14 +26,21 @@ public class ModuleValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "crsms.modules.error.name.required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "crsms.error.description.required");
 		
-		Module module = (Module) target;
+		ModuleFormDto moduleFormDto = (ModuleFormDto) target;
 		
-		if (module.getName().length() > Module.MAX_NAME_LENGTH) {
+		for (Module module : courseService.getCourseById(moduleFormDto.getCourseId()).getModules()) {
+			if (module.getName().equalsIgnoreCase(moduleFormDto.getName())) {
+				errors.rejectValue("name", "crsms.error.not.unique.name");
+				break;
+			}
+		}
+		
+		if (moduleFormDto.getName().length() > Module.MAX_NAME_LENGTH) {
 			errors.rejectValue("name", "crsms.error.too.long", 
 								new Object[]{Module.MAX_NAME_LENGTH}, "name is too long");
 		}
 		
-		if (module.getDescription().length() > Module.MAX_DESCTIPTION_LENGTH) {
+		if (moduleFormDto.getDescription().length() > Module.MAX_DESCTIPTION_LENGTH) {
 			errors.rejectValue("description", "crsms.error.too.long", 
 								new Object[]{Module.MAX_DESCTIPTION_LENGTH}, "description is too long");
 		}
