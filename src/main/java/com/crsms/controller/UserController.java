ffb3239e23fb17be467b1a1a7f740aa1603b62e6
yuliia <uuuuu@ooo.ff>
2015-11-05 16:08:56 +0200

    Add validation for User,UserInfo, add encode to changePassword function

--- src/main/java/com/crsms/controller/UserController.java
+++ src/main/java/com/crsms/controller/UserController.java
@@ -5,15 +5,23 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.crsms.domain.User;
+import com.crsms.domain.UserInfo;
 import com.crsms.service.RoleService;
 import com.crsms.service.UserInfoService;
 import com.crsms.service.UserService;
+import com.crsms.validator.UserInfoValidator;
+import com.crsms.validator.UserValidator;
 
 @Controller
 public class UserController {
@@ -28,31 +36,53 @@ public class UserController {
 	
 	@Autowired
 	private RoleService roleService;
+	
+	@Autowired
+	private UserValidator userValidator;
+	
+	@Autowired
+	private UserInfoValidator userInfoValidator;
+	
+	@InitBinder("user")
+    private void initUserBinder(WebDataBinder binder) {
+		binder.setValidator(userValidator);
+    }
 
-	@RequestMapping("/signUp")
+	@InitBinder("userInfo")
+    private void initUserInfoBinder(WebDataBinder binder) {
+		binder.setValidator(userInfoValidator);
+    }
+	
+	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
 	public String signUp(Model model) {
+		model.addAttribute("userRegistr", new User());
 		return "signUp";
 	};
-
+	
 	@RequestMapping(value = "/submitUser", method = RequestMethod.POST)
-	public String submitUser(Model model, HttpSession session,
-			@RequestParam("email") String email, @RequestParam("password") String password) {
-		userService.createUser(email, password, STUDENT_ROLE_ID);
-		session.setAttribute("email", email);
-		return "redirect:/userProfile"; ///"login";
+	public String submitUser(@Validated @ModelAttribute("userRegistr")  User user, BindingResult result, HttpSession session) {
+		user.setRole(roleService.getRoleById(STUDENT_ROLE_ID));
+		if (result.hasErrors()) {
+			return "signUp";
+		}
+		userService.saveUser(user);
+		session.setAttribute("email", user.getEmail());
+		return "redirect:/userProfile"; ///   !!!!"login";
 	};
 	
 	@RequestMapping("/userProfile")
-	public String userProfile() {
+	public String userProfile(Model model) {
+		model.addAttribute(new UserInfo());
 		return "userProfile";
 	}
 
 	@RequestMapping(value = "/submitUserInfo", method = RequestMethod.POST)
-	public String submitUserInfo(Model model,
-			@RequestParam("fName") String fName,
-			@RequestParam("sName") String sName,
-			@ModelAttribute("email") String email) {
-		userInfoService.createUserInfo(fName, sName, email);
+	public String submitUserInfo(@Validated @ModelAttribute("userInfo") UserInfo userInfo, BindingResult result, @ModelAttribute("email") String email) {
+		userInfo.setUser(userService.getUserByEmail(email));
+		if (result.hasErrors()) {
+			return "userProfile";
+		}
+		userInfoService.saveUserInfo(userInfo);
 		return "redirect:/signUp"; //"logout"
 	}
 	
---
+++ src/main/java/com/crsms/validator/UserValidator.java
@@ -0,0 +1,52 @@
+package com.crsms.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.crsms.domain.User;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class UserValidator implements Validator {
+
+	@Override
+	public boolean supports(Class clazz) {
+		return User.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "crsms.error.email.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "crsms.error.password.required");
+		
+		User user = (User) obj;
+		
+		Matcher matcher;
+		final String EMAIL_PATTERN = 
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+				matcher = pattern.matcher(user.getEmail());
+		
+		if(!matcher.matches()){
+			errors.rejectValue("email", "crsms.error.email.invalid",
+					new Object[] { User.BY_EMAIL },
+					"Incorrect Email format");
+		};
+
+		if (user.getPassword().length() > User.MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("password", "crsms.error.too.long",
+					new Object[] { User.MAX_PASSWORD_LENGTH },
+					"Password is too long");
+		} else if (user.getPassword().length() < User.MIN_PASSWORD_LENGTH) {
+			errors.rejectValue("password", "crsms.error.too.short",
+					new Object[] { User.MIN_PASSWORD_LENGTH },
+					"Password is too short");
+		}
+
+	}
+
+}
