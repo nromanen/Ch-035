428de2cc7831c0e9a7c7e16efdd83503ee5e169e
valeriimotresku <valeriimotresku@gmail.com>
2015-11-17 17:41:28 +0200

    Refactoring

--- src/main/java/com/crsms/service/FileService.java
+++ src/main/java/com/crsms/service/FileService.java
@@ -1,12 +1,15 @@
 package com.crsms.service;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public interface MultipartFileService {
+public interface FileService {
 	
 	void uploadFile(MultipartFile multipartFile) throws IOException;
+	
+	File getFileFromStorage(String name);
 	
 	String getStoragePath();
 
