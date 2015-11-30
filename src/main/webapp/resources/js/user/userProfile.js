0108dc5f81eb5c89bec6bf0b2980d5f00b91673b
georgekucher <gonzo-2005@yandex.ru>
2015-11-30 16:29:44 +0200

    and (UI) validation for all

--- src/main/webapp/resources/js/user/userProfile.js
+++ src/main/webapp/resources/js/user/userProfile.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
 	$("#changePasswordBtn").click(
 		function() {
-			var url = "changePassword?currentPass="
-					+ $("#currentPass").val() + "&newPassword="
-					+ $("#newPassword").val() + "&_csrf="
-					+ $("#csrf").val();
-			$.post(url).done(function(response) {
-				if (response == 'Fail') {
-					alert("Enter correct current password");
-				} else {
-					$("#closeModalBtn").click();
-				}
-			}).fail(function() {
-				alert("Failed to submit form");
-			});
+			if ($('#changePasswordForm').valid()) {
+				var url = "changePassword?currentPass="
+						+ $("#currentPass").val() + "&newPassword="
+						+ $("#newPassword").val() + "&_csrf="
+						+ $("#csrf").val();
+				$.post(url).done(function(response) {
+					if (response == 'Fail') {
+						alert("Enter correct current password");
+					} else {
+						$("#closeModalBtn").click();
+					}
+				}).fail(function() {
+					alert("Failed to submit form");
+				});
+			}
 		});
 	
 	$("#pic").change(function() {
@@ -38,6 +40,19 @@ $(document).ready(function() {
             },
             "lastName": {
         		regexName: '^[^<>$%~`!@#\\^&*()_+=\\{\\}\\[\\]\\.,|;:"?/\\d\\\\]{1,40}$'
+            },
+        },
+    });
+	
+	$('#changePasswordForm').validate({
+        errorClass: "errorTxt",
+		rules: {
+			"currentPass": {
+        		required: true,
+        		minlength: 2
+            },
+            "newPassword": {
+            	required: true
             },
         },
     });
