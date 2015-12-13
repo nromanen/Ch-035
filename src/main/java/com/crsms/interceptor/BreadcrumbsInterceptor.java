0a362311b8807e5bf2e2ec5962dfd7e8689b29db
stefankiv <stefankiv@inbox.ru>
2015-12-13 13:59:00 +0200

    added course's, module's etc. names into breadcrumbs

--- src/main/java/com/crsms/interceptor/BreadcrumbsInterceptor.java
+++ src/main/java/com/crsms/interceptor/BreadcrumbsInterceptor.java
@@ -1,5 +1,6 @@
 package com.crsms.interceptor;
 
+import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -8,28 +9,42 @@ import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.crsms.util.BreadcrumbsHelper;
+import com.crsms.util.StringUtil;
 
 public class BreadcrumbsInterceptor extends HandlerInterceptorAdapter {
-	private static final String REGEXP_PARSE_URL = "([\\d]+\\/)?\\w+[\\/]?$";
-	private static final String REGEXP_REMOVE_NUMBERS = "\\d*\\/";
+	private static final Boolean LAST_WORD = true;
+	private static final Integer MAX_NAME_LENGTH = 10;
+	private static final String REGEXP_PARSE_URL = "\\w+[\\/]?$";
+	private static final String REGEXP_IS_NUMBER = "\\d+";
 	private static final String REGEXP_REMOVE_SLASHES = "\\/";
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response,
-							Object handler, ModelAndView modelAndView)
-							throws Exception {		
-
+			Object handler, ModelAndView modelAndView) throws Exception {		
 		String url = request.getRequestURI();
-		modelAndView.addObject("breadcrumbs", makeBreadcrumbs(url));
+		Locale locale = LocaleContextHolder.getLocale();
+		modelAndView.addObject("breadcrumbs", makeBreadcrumbs(url, locale));
 	}
 	
-	private Map<String, String> makeBreadcrumbs(String url) {
+	private Map<String, String> makeBreadcrumbs(String url, Locale locale) {
+		Map<String, String> breadcrumbs = splitUrl(url);
+		
+		//remove first entry (crsms)
+		((TreeMap<String, String>) breadcrumbs).pollFirstEntry();
+		
+		setBreadcrumbsLabels(breadcrumbs, locale);
+		
+		return breadcrumbs;
+	}
+
+	private Map<String, String> splitUrl(String url) {
 		String path = new String(url);
-		Map<String, String> breadcrumbs = new TreeMap<>();
+		Map<String, String> map = new TreeMap<>();
 		
 		Pattern pattern = Pattern.compile(REGEXP_PARSE_URL);
 		Matcher matcher = pattern.matcher(path);
@@ -37,19 +52,39 @@ public class BreadcrumbsInterceptor extends HandlerInterceptorAdapter {
 		String match = null;
 		while (matcher.find()) {
 			match = matcher.group();
-			breadcrumbs.put(path, makeLabel(match));
+			map.put(path, removeSlashes(match));
 			path = removeMatch(path, match);
 			matcher = pattern.matcher(path);
 		}
-		((TreeMap<String, String>) breadcrumbs).pollFirstEntry();
-		return breadcrumbs;
+		return map;
+	}
+	
+	private void setBreadcrumbsLabels(Map<String, String> breadcrumbs, Locale locale) {
+		String target = null;
+		for (String key : breadcrumbs.keySet()) {
+			String currentCrumb = breadcrumbs.get(key);
+			
+			if (!isNumber(currentCrumb)) {
+				target = breadcrumbs.get(key);
+				breadcrumbs.put(key, BreadcrumbsHelper.getLabel(currentCrumb, locale));
+			} else {
+				breadcrumbs.put(key, getTargetName(target, currentCrumb));
+			}
+		}
 	}
 
-	private String makeLabel(String target) {
-		String label = target.replaceAll(REGEXP_REMOVE_NUMBERS, "")
-							 .replaceAll(REGEXP_REMOVE_SLASHES, "");
-		return BreadcrumbsHelper.getCode(label);
-	}	
+	private String getTargetName(String target, String id) {
+		return StringUtil.trimString(BreadcrumbsHelper.getName(target, id),
+				MAX_NAME_LENGTH, LAST_WORD);
+	}
+	
+	private boolean isNumber(String string) {
+		return string.matches(REGEXP_IS_NUMBER);
+	}
+	
+	private String removeSlashes(String target) {
+		return target.replaceAll(REGEXP_REMOVE_SLASHES, "");
+	}
 	
 	private String removeMatch(String path, String match) {
 		return path.substring(0, path.lastIndexOf(match));
--- src/main/java/com/crsms/util/BreadcrumbsHelper.java
+++ src/main/java/com/crsms/util/BreadcrumbsHelper.java
@@ -1,44 +1,116 @@
 package com.crsms.util;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.web.context.ContextLoader;
+
+import com.crsms.service.AreaService;
+import com.crsms.service.CourseService;
+import com.crsms.service.GroupService;
+import com.crsms.service.ModuleService;
+import com.crsms.service.ResourceService;
+import com.crsms.service.TestService;
+
 public class BreadcrumbsHelper {
+	public static final String LABEL_CRSMS = "crsms";
+	public static final String CODE_CRSMS = "crsms.bc.crsms";
+	public static final String LABEL_AREAS = "areas";
+	public static final String CODE_AREAS = "crsms.bc.areas";
+	public static final String LABEL_COURSES = "courses";
+	public static final String CODE_COURSES = "crsms.bc.courses";
+	public static final String LABEL_MODULES = "modules";
+	public static final String CODE_MODULES = "crsms.bc.modules";
+	public static final String LABEL_GROUPS = "groups";
+	public static final String CODE_GROUPS = "crsms.bc.groups";
+	public static final String LABEL_TESTS = "tests";
+	public static final String CODE_TESTS = "crsms.bc.tests";
+	public static final String LABEL_RESOURCES = "resources";
+	public static final String CODE_RESOURCES = "crsms.bc.resources";
+	public static final String LABEL_ADD = "add";
+	public static final String CODE_ADD = "crsms.bc.add";
+	public static final String LABEL_EDIT = "edit";
+	public static final String CODE_EDIT = "crsms.bc.edit";
+	public static final String LABEL_DELETE = "delete";
+	public static final String CODE_DELETE = "crsms.bc.delete";
+	public static final String LABEL_SIGNIN = "signin";
+	public static final String CODE_SIGNIN = "crsms.bc.signin";
+	public static final String LABEL_SIGNUP = "signup";
+	public static final String CODE_SIGNUP = "crsms.bc.signup";
+	public static final String LABEL_ADMIN = "admin";
+	public static final String CODE_ADMIN = "crsms.bc.admin";
+	public static final String LABEL_USER_PROFILE = "userprofile";
+	public static final String CODE_USER_PROFILE = "crsms.bc.userprofile";
+	public static final String LABEL_SEARCH = "search";
+	public static final String CODE_SEARCH = "crsms.bc.search";
+
+	public static final String CODE_UNKNOWN = "crsms.bc.unknown";
 	
-	public static final String CRSMS = "crsms.bc.crsms";
-	public static final String AREAS = "crsms.bc.areas";
-	public static final String COURSES = "crsms.bc.courses";
-	public static final String MODULES = "crsms.bc.modules";
-	public static final String GROUPS = "crsms.bc.groups";
-	public static final String TESTS = "crsms.bc.tests";
-	public static final String RESOURCES = "crsms.bc.resources";
-	public static final String ADD = "crsms.bc.add";
-	public static final String EDIT = "crsms.bc.edit";
-	public static final String DELETE = "crsms.bc.delete";
-	public static final String SIGNIN = "crsms.bc.signin";
-	public static final String SIGNUP = "crsms.bc.signup";
-	public static final String ADMIN = "crsms.bc.admin";
-	public static final String USER_PROFILE = "crsms.bc.userprofile";
-	public static final String SEARCH = "crsms.bc.search";
+	private static final String MESSAGE_SOURCE_BEAN_NAME = "messageSource";
+	private static final String AREA_SERVICE_BEAN_NAME = "areaService";
+	private static final String COURSE_SERVICE_BEAN_NAME = "courseService";
+	private static final String MODULE_SERVICE_BEAN_NAME = "moduleService";
+	private static final String TEST_SERVICE_BEAN_NAME = "testService";
+	private static final String RESOURCE_SERVICE_BEAN_NAME = "resourceService";
+	private static final String GROUP_SERVICE_BEAN_NAME = "groupService";
 	
-	public static final String UNKNOWN = "crsms.bc.unknown";
+	private static MessageSource messageSource = (MessageSource)
+			ContextLoader.getCurrentWebApplicationContext().getBean(MESSAGE_SOURCE_BEAN_NAME);
+	
+	private static AreaService areaService = (AreaService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(AREA_SERVICE_BEAN_NAME);
+	
+	private static CourseService courseService = (CourseService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(COURSE_SERVICE_BEAN_NAME);
+	
+	private static ModuleService moduleService = (ModuleService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(MODULE_SERVICE_BEAN_NAME);
+	
+	private static TestService testService = (TestService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(TEST_SERVICE_BEAN_NAME);
+	
+	private static ResourceService resourceService = (ResourceService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(RESOURCE_SERVICE_BEAN_NAME);
+	
+	private static GroupService groupService = (GroupService)
+			ContextLoader.getCurrentWebApplicationContext().getBean(GROUP_SERVICE_BEAN_NAME);
 
 	public static String getCode(String label) {
 		switch (label) {
-			case "crsms": return CRSMS;
-			case "areas": return AREAS;
-			case "courses": return COURSES;
-			case "modules": return MODULES;
-			case "tests": return TESTS;
-			case "resources": return RESOURCES;
-			case "groups": return GROUPS;
-			case "add": return ADD;
-			case "edit": return EDIT;
-			case "delete": return DELETE;
-			case "signin": return SIGNIN;
-			case "signUp": return SIGNUP;
-			case "admin": return ADMIN;
-			case "userProfile": return USER_PROFILE;
-			case "search": return SEARCH;
-			default: return UNKNOWN;
+			case LABEL_CRSMS: return CODE_CRSMS;
+			case LABEL_AREAS: return CODE_AREAS;
+			case LABEL_COURSES: return CODE_COURSES;
+			case LABEL_MODULES: return CODE_MODULES;
+			case LABEL_TESTS: return CODE_TESTS;
+			case LABEL_RESOURCES: return CODE_RESOURCES;
+			case LABEL_GROUPS: return CODE_GROUPS;
+			case LABEL_ADD: return CODE_ADD;
+			case LABEL_EDIT: return CODE_EDIT;
+			case LABEL_DELETE: return CODE_DELETE;
+			case LABEL_SIGNIN: return CODE_SIGNIN;
+			case LABEL_SIGNUP: return CODE_SIGNUP;
+			case LABEL_ADMIN: return CODE_ADMIN;
+			case LABEL_USER_PROFILE: return CODE_USER_PROFILE;
+			case LABEL_SEARCH: return CODE_SEARCH;
+			default: return CODE_UNKNOWN;
 		}
+	}
+	
+	public static String getName(String target, String id) {
+		Long targetId = Long.parseLong(id);
+		switch (target) {
+			case LABEL_AREAS: return areaService.getById(targetId).getName();
+			case LABEL_COURSES: return courseService.getById(targetId).getName();
+			case LABEL_MODULES: return moduleService.getById(targetId).getName();
+			case LABEL_TESTS: return testService.getById(targetId).getName();
+			case LABEL_RESOURCES: return resourceService.getById(targetId).getName();
+			case LABEL_GROUPS: return groupService.getById(targetId).getName();
+			default: return "";
+		}
+	}
+	
+	public static String getLabel(String target, Locale locale) {
+		return messageSource.getMessage(getCode(target), new Object[] {}, locale);
 	}
 	
 }
