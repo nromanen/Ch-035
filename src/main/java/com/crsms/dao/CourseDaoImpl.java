83a88c0c4fdf355345b458aefe946c658004ef78
stefankiv <stefankiv@inbox.ru>
2015-12-16 16:10:50 +0200

    removed empty lists

--- src/main/java/com/crsms/dao/CourseDaoImpl.java
+++ src/main/java/com/crsms/dao/CourseDaoImpl.java
@@ -1,12 +1,10 @@
 package com.crsms.dao;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
 
@@ -27,43 +25,16 @@ public class CourseDaoImpl extends BaseDaoImpl<Course> implements CourseDao {
 		super(Course.class);
 	}
 	
-	@Override
-	public Course get(String name) {
-		try {
-			getSessionFactory().getCurrentSession()
-				.createQuery("FROM Course c WHERE c.name=:name")
-				.setString("name", name).uniqueResult();
-		} catch (Exception e) {
-			getLogger().error("Error getCourse: " + e);
-			throw e;
-		}
-		return null;
-	}
-
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Course> getAllByAreaId(Long areaId) {
 		try {
-			String hql = "from Course where area_id = :id order by id asc";
-			Query query = getSessionFactory()
-							  .getCurrentSession()
-							  .createQuery(hql).setParameter("id", areaId);
-			return query.list();
+			return getSessionFactory().getCurrentSession()
+									  .getNamedQuery(Course.GET_BY_AREA_ID)
+							  		  .setParameter("id", areaId)
+							  		  .list();
 		} catch (Exception e) {
 			getLogger().error("Error in getting all courses by area id: " + e);
-			throw e;
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Course> getAllByUserId(Long userId) {
-		try {
-			return getSessionFactory().getCurrentSession()
-										   .getNamedQuery(Course.GET_BY_USER_ID)
-									 	   .setParameter("userId", userId).list();
-		} catch (Exception e) {
-			getLogger().error("Error in getting all courses by user id: " + e);
 			throw e;
 		}
 	}
@@ -73,8 +44,8 @@ public class CourseDaoImpl extends BaseDaoImpl<Course> implements CourseDao {
 	public List<Course> getAllByUserEmail(String email) {
 		try {
 			return getSessionFactory().getCurrentSession()
-										   .getNamedQuery(Course.GET_BY_USER_EMAIL)
-									 	   .setParameter("email", email).list();
+									  .getNamedQuery(Course.GET_BY_USER_EMAIL)
+									  .setParameter("email", email).list();
 		} catch (Exception e) {
 			getLogger().error("Error in getting all courses by user email: " + e);
 			throw e;
@@ -86,8 +57,8 @@ public class CourseDaoImpl extends BaseDaoImpl<Course> implements CourseDao {
 	public List<Course> getAllByOwnerEmail(String email) {
 		try {
 			return getSessionFactory().getCurrentSession()
-										   .getNamedQuery(Course.GET_BY_OWNER_EMAIL)
-									 	   .setParameter("email", email).list();
+									  .getNamedQuery(Course.GET_BY_OWNER_EMAIL)
+									  .setParameter("email", email).list();
 		} catch (Exception e) {
 			getLogger().error("Error in getting all courses by owner email: " + e);
 			throw e;
@@ -98,9 +69,10 @@ public class CourseDaoImpl extends BaseDaoImpl<Course> implements CourseDao {
 	@Override
 	public List<Course> searchCourses(String searchWord) {
 		try {
-			return (List<Course>) getSessionFactory().getCurrentSession()
+			return getSessionFactory().getCurrentSession()
 									  .getNamedQuery(Course.SEARCH)
-									  .setParameter("s", "%" + searchWord + "%").list();
+									  .setParameter("s", "%" + searchWord + "%")
+									  .list();
 		} catch (HibernateException e) {
 			getLogger().error("Error searchCourses: " + e);
 			throw e;
@@ -138,18 +110,17 @@ public class CourseDaoImpl extends BaseDaoImpl<Course> implements CourseDao {
 	@SuppressWarnings("unchecked")
 	@Override
 	public Map<Long, Long> getStudentCoursesAndGroupsIds(String email) {
-		List<List<Long>> list = new ArrayList<>();
 		try {
-			list = this.getSessionFactory().getCurrentSession()
-										   .getNamedQuery(Course.GET_STUDENT_COURSES_AND_GROUPS_IDS)
-									 	   .setParameter("email", email)
-									 	   .setResultTransformer(Transformers.TO_LIST)
-									 	   .list();
+			List<List<Long>> list = this.getSessionFactory().getCurrentSession()
+										.getNamedQuery(Course.GET_STUDENT_COURSES_AND_GROUPS_IDS)
+									 	.setParameter("email", email)
+									 	.setResultTransformer(Transformers.TO_LIST)
+									 	.list();
+			return transformListToMap(list);
 		} catch (Exception e) {
 			this.getLogger().error("Error in getting student's courses and groups id's: " + e);
 			throw e;
 		}
-		return transformListToMap(list);
 	}
 	
 	/**
--- src/main/java/com/crsms/dao/GroupDaoImpl.java
+++ src/main/java/com/crsms/dao/GroupDaoImpl.java
@@ -1,6 +1,5 @@
 package com.crsms.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -15,28 +14,24 @@ public class GroupDaoImpl extends BaseDaoImpl<Group> implements GroupDao {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Group> getAllByCourseId(Long courseId) {
-		List<Group> groups = new ArrayList<>();
 		try {
-			groups = this.getSessionFactory()
-						 .getCurrentSession()
-						 .getNamedQuery(Group.GET_ALL_BY_COURSE_ID)
-						 .setParameter("courseId", courseId)
-						 .list();
+			return getSessionFactory().getCurrentSession()
+									  .getNamedQuery(Group.GET_ALL_BY_COURSE_ID)
+									  .setParameter("courseId", courseId)
+									  .list();
 		} catch (Exception e) {
 			this.getLogger().error("Error in getting all groups by course id: " + e);
 			throw e;
 		}
-		return groups;
 	}
 
 	@Override
 	public void deleteById(Long groupId) {
 		try {
-			this.getSessionFactory()
-				.getCurrentSession()
-				.getNamedQuery(Group.DELETE_BY_ID)
-				.setParameter("id", groupId)
-				.executeUpdate();
+			this.getSessionFactory().getCurrentSession()
+									.getNamedQuery(Group.DELETE_BY_ID)
+									.setParameter("id", groupId)
+									.executeUpdate();
 		} catch (Exception e) {
 			getLogger().error("Error in delete group by id: " + e);
 			throw e;
@@ -47,11 +42,10 @@ public class GroupDaoImpl extends BaseDaoImpl<Group> implements GroupDao {
 	@Override
 	public List<UserIdFNameLNameEmailDto> getStudentsFromGroup(Long groupId) {
 		try {
-			return this.getSessionFactory()
-					   .getCurrentSession()
-					   .getNamedQuery(Group.GET_STUDENTS_FROM_GROUP)
-					   .setParameter("id", groupId)
-					   .list();
+			return getSessionFactory().getCurrentSession()
+									  .getNamedQuery(Group.GET_STUDENTS_FROM_GROUP)
+									  .setParameter("id", groupId)
+									  .list();
 		} catch (Exception e) {
 			getLogger().error("Error in get students id and email from group: " + e);
 			throw e;
@@ -62,12 +56,11 @@ public class GroupDaoImpl extends BaseDaoImpl<Group> implements GroupDao {
 	@Override
 	public List<String> selectAlreadySubscribedUsers(Long courseId, Set<String> emails) {
 		try {
-			return this.getSessionFactory()
-					   .getCurrentSession()
-					   .getNamedQuery(Group.SELECT_ALREADY_SUBSCRIBED_USERS)
-					   .setParameter("courseId", courseId)
-					   .setParameterList("emails", emails)
-					   .list();
+			return getSessionFactory().getCurrentSession()
+									  .getNamedQuery(Group.SELECT_ALREADY_SUBSCRIBED_USERS)
+									  .setParameter("courseId", courseId)
+									  .setParameterList("emails", emails)
+									  .list();
 		} catch (Exception e) {
 			getLogger().error("Error in select already subscribed users: " + e);
 			throw e;
@@ -78,11 +71,10 @@ public class GroupDaoImpl extends BaseDaoImpl<Group> implements GroupDao {
 	@Override
 	public List<UserIdFNameLNameEmailDto> searchStudents(String textToSearch) {
 		try {
-			return this.getSessionFactory()
-					   .getCurrentSession()
-					   .getNamedQuery(Group.SEARCH_STUDENTS)
-					   .setParameter("textToSearch", "%" + textToSearch + "%")
-					   .list();
+			return getSessionFactory().getCurrentSession()
+									  .getNamedQuery(Group.SEARCH_STUDENTS)
+									  .setParameter("textToSearch", "%" + textToSearch + "%")
+									  .list();
 		} catch (Exception e) {
 			getLogger().error("Error in search students: " + e);
 			throw e;
